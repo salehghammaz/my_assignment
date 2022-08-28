fbcf7e0f1d7e5e2f70ca6ba3152bc9924bcf4b6f
salehghammaz <salehghammaz36@gmail.com>
2022-08-28 03:20:23 +0300

    myAssgiment

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  Map myInfo = 
+  {
+    "name":"saleh",
+    "age":22,
+    "education_info":
+    {
+      "university_name":"BAU",
+      "faculty":"IT",
+      "graduation_year":2022
+
+    }
+  };
+  print(myInfo);
+}
